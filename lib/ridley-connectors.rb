1cbd32743e2209241ab2a7d4aceceda19915b40c
Kyle Allan <kallan@riotgames.com>
2013-11-15 16:29:01 -0800

    Moving node specific code out of Ridley.
    Move Connector code to this gem's NodeObject and NodeResource
    Move spec code to this gem from Ridley

--- lib/ridley-connectors.rb
+++ lib/ridley-connectors.rb
@@ -5,4 +5,6 @@ require 'ridley'
 module Ridley
   require_relative 'ridley-connectors/host_commander'
   require_relative 'ridley-connectors/host_connector'
+  require_relative 'ridley-connectors/chef_objects/node_object'
+  require_relative 'ridley-connectors/resources/node_resource'
 end
